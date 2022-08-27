848e99370d18c912905c7a0ba5bbcb61094670aa
wooseokoh <allsafe1206@gmail.com>
2022-08-27 13:11:46 +0900

    BookService BookList Complete

--- src/main/java/site/junit/junitproject/service/BookService.java
+++ src/main/java/site/junit/junitproject/service/BookService.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import site.junit.junitproject.domain.Book;
 import site.junit.junitproject.domain.BookRepository;
@@ -21,5 +25,22 @@ public class BookService {
     public BookRespDto 책등록하기(BookSaveReqDto dto) {
         Book bookPS = bookRepository.save(dto.toEntity());
         return new BookRespDto().toDto(bookPS);
+    }
+
+    // 2. 책목록보기
+    public List<BookRespDto> 책목록보기() {
+
+        List<BookRespDto> dtos = bookRepository.findAll().stream()
+                // .map((bookPS) -> bookPS.toDto())
+                .map(Book::toDto)
+                .collect(Collectors.toList());
+
+        dtos.stream().forEach((b) -> {
+            System.out.println(b.getId());
+            System.out.println(b.getTitle());
+            System.out.println("============ 서비스 레이어");
+        });
+
+        return dtos;
     }
 }
