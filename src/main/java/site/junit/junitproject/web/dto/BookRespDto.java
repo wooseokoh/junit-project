51601220ffa469435174b3c2ec8e30f21ac533b0
wooseokoh <allsafe1206@gmail.com>
2022-08-29 00:03:36 +0900

    BookService update Test Complete

--- src/main/java/site/junit/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/junit/junitproject/web/dto/BookRespDto.java
@@ -3,7 +3,6 @@ package site.junit.junitproject.web.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import site.junit.junitproject.domain.Book;
 
 @NoArgsConstructor
 @Getter
