16727601235b35110302ca61ba7ecf369563e6d6
wooseokoh <allsafe1206@gmail.com>
2022-09-20 01:11:51 +0900

    BookController saveBook_test

--- src/test/java/site/junit/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/junit/junitproject/web/BookApiControllerTest.java
@@ -1,5 +1,78 @@
 package site.junit.junitproject.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import site.junit.junitproject.service.BookService;
+import site.junit.junitproject.web.dto.request.BookSaveReqDto;
+
+// 통합테스트 (C, S, R)
+// 컨트롤러만 테스트하는 것이 아님
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BookApiControllerTest {
+
+    @Autowired
+    private BookService bookservice;
+
+    @Autowired
+    private TestRestTemplate rt;
+
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("1");
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void saveBook_test() throws Exception {
+        // given
+        BookSaveReqDto bookSaveReqDto = new BookSaveReqDto();
+        bookSaveReqDto.setTitle("스프링1강");
+        bookSaveReqDto.setAuthor("겟인데어");
+
+        String body = om.writeValueAsString(bookSaveReqDto);
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.POST, request, String.class);
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("스프링1강");
+        assertThat(author).isEqualTo("겟인데어");
+
+    }
+
+    // @Test
+    // public void di_test() {
+    // if (bookservice == null) {
+    // System.out.println("null입니다.");
+    // } else {
+    // System.out.println("null아닙니다.");
+    // }
+    // }
 
 }
